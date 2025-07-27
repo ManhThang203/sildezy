8e5551a5032db7695313114ab8175c63736a8574
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2025-07-27 22:00:58 +0700

    Xây dựng phương thức moveSlide

--- js/main.js
+++ js/main.js
@@ -1,60 +1,63 @@
-function Slidezy(selector, options = {}) {
-  this.container = document.querySelector(selector);
+function Slidezy(select, option = {}) {
+  this.container = document.querySelector(select);
   if (!this.container) {
-    console.error(`Slider: Container ${selector} not found!`);
+    console.error(`Slider: ${this.container} dont next`);
     return;
   }
 
-  this.opt = Object.assign({},options);
-
+  this.opt = Object.assign({}, option);
   this.slides = Array.from(this.container.children);
+
+  this.currentIndex = 0;
   this._init();
 }
 
-Slidezy.prototype._init = function(){
-   this.container.className = "slidezy-wrapper";
-   console.log(this.container);
-   this._createTrack();
-   this._createNavigation();
-}
+Slidezy.prototype._init = function () {
+  this.container.className = "slide-wrapper";
+  console.log(this.container);
 
-Slidezy.prototype._createTrack = function(){
-   this.track = document.createElement("div");
+  this._creatTrack();
+  this._createNavigation();
+};
 
-   this.track.className = "slidezy-track";
+Slidezy.prototype._creatTrack = function () {
+  this.track = document.createElement("div");
+  this.track.className = "slide-track";
 
-   this.slides.forEach((slide) => {
-      slide.className = "slidezy-slide";
-      this.track.appendChild(slide);
-   })
-   this.container.appendChild(this.track);
-}
+  this.slides.forEach((slide) => {
+    slide.className = "slide-item";
+    this.track.appendChild(slide);
+  });
+  this.container.appendChild(this.track);
+};
 
+Slidezy.prototype._createNavigation = function () {
+  this.preBtn = document.createElement("button");
+  this.nextBtn = document.createElement("button");
 
-Slidezy.prototype._createNavigation = function(){
-    this.prevBtn = document.createElement("button");
-    this.nextBtn = document.createElement("button");
+  this.preBtn.className = "pre-slide";
+  this.nextBtn.className = "next-slide";
 
-    this.prevBtn.className = "slidezy-prev";
-    this.nextBtn.className = "slidezy-next";
+  this.iconPre = document.createElement("i");
+  this.iconNext = document.createElement("i");
 
-    this.iconPrev = document.createElement("i")
-    this.iconNext = document.createElement("i")
+  this.iconPre.className = "fa-solid fa-angle-left";
+  this.iconNext.className = "fa-solid fa-angle-right";
 
-    this.iconPrev.className = "fa-solid fa-angle-left";
-    this.iconNext.className = "fa-solid fa-angle-right";
+  this.preBtn.appendChild(this.iconPre);
+  this.nextBtn.appendChild(this.iconNext);
 
-    this.prevBtn.appendChild(this.iconPrev);
-    this.nextBtn.appendChild(this.iconNext);
+  this.container.append(this.preBtn, this.nextBtn);
 
-    this.container.append(this.prevBtn,this.nextBtn);
+  this.preBtn.onclick = () => this._moveSlide(-1);
+  this.nextBtn.onclick = () => this._moveSlide(1);
+};
 
-    this.prevBtn.onclick = () => this._moveSlide(-1);
-    this.nextBtn.onclick = () => this._moveSlide(1);
-}
-
-
-Slidezy.prototype._moveSlide = function(step){
-   console.log(step);
-}
-
+Slidezy.prototype._moveSlide = function (step) {
+  this.currentIndex = Math.min(
+    Math.max(this.currentIndex + step, 0),
+    this.slides.length - 3
+  );
+   this.offset = `-${this.currentIndex * (100 / 3)}`;
+   this.track.style.transform = `translateX(${this.offset}%)`;
+};
